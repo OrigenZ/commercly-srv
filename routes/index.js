60acbde7a1fdf0429e7803d0131cae9ecd500f5e
Sherab Pereira <sherabpereira@gmail.com>
2021-09-29 21:39:27 +0200

    Fix: JTW payload on login

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,3 @@
 const router = require('express').Router()
 
-/* GET home page */
-router.get('/', (req, res, next) => {
-  // res.render('index', { totalItemsCart: req.session.totalItems })
-  res.render('index')
-})
-
 module.exports = router
