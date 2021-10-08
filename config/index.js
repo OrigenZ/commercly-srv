fac7a71702d1805eb4043f947f2b55e7838a0e61
Sherab Pereira <sherabpereira@gmail.com>
2021-10-08 03:09:30 +0200

    .

--- config/index.js
+++ config/index.js
@@ -16,7 +16,7 @@ module.exports = (app) => {
   app.use(
     cors({
       credentials: true,
-      origin: process.env.ORIGIN || 'http://localhost:5005/',
+      origin: process.env.ORIGIN || 'http://localhost:5005',
       // origin: '*'
     }),
   )
