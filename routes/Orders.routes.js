2f01b4b4e978b6ec96738ecd932be7268615823c
Sherab Pereira <sherabpereira@gmail.com>
2022-01-02 13:51:31 +0100

    refactor Orders.routes to async/await

--- routes/Orders.routes.js
+++ routes/Orders.routes.js
@@ -5,64 +5,97 @@ const Order = require('../models/Order.model')
 
 //GET /api/orders - Get all orders from the database
 router.get('/', async (req, res, next) => {
-  Order.find()
-    .populate('customer')
-    .then((orders) => res.status(200).json(orders))
-    .catch((err) => next(err))
+  const orders = await Order.find().populate('customer')
+  try {
+    res.status(200).json(orders)
+  } catch (err) {
+    next(err)
+  }
 })
 
 //GET /api/orders/:orderId - Get an order by order id from the database
-router.get('/:orderId', (req, res, next) => {
+router.get('/:orderId', async (req, res, next) => {
   const { orderId } = req.params
-
-  Order.findById(orderId)
-    .populate('customer orderLines.productId')
-    .then((orders) => res.status(200).json(orders))
-    .catch((err) => next(err))
+  try {
+    const orders = await Order.findById(orderId).populate(
+      'customer orderLines.productId',
+    )
+    res.status(200).json(orders)
+  } catch (err) {
+    next(err)
+  }
 })
 
 //GET /api/orders/customer/:customerId - Get orders by customer id from the database
-router.get('/customer/:customerId', (req, res, next) => {
+router.get('/customer/:customerId', async (req, res, next) => {
   const { customerId } = req.params
-
-  Order.find({ customer: customerId })
-    .populate('customer')
-    .then((orders) => res.status(200).json(orders))
-    .catch((err) => next(err))
+  try {
+    const orders = await Order.find({ customer: customerId }).populate(
+      'customer',
+    )
+    res.status(200).json(orders)
+  } catch (err) {
+    next(err)
+  }
 })
 
 //POST /api/orders - Creates an order in the database
-router.post('/', (req, res, next) => {
-  const { customer, status, shippingFees, orderLines, totalTaxes, totalOrder } = req.body
-
-  Order.create({ customer, status, shippingFees, orderLines, totalTaxes, totalOrder })
-    .then((orders) => res.status(200).json(orders))
-    .catch((err) => next(err))
+router.post('/', async (req, res, next) => {
+  const {
+    customer,
+    status,
+    shippingFees,
+    orderLines,
+    totalTaxes,
+    totalOrder,
+  } = req.body
+  try {
+    const orders = await Order.create({
+      customer,
+      status,
+      shippingFees,
+      orderLines,
+      totalTaxes,
+      totalOrder,
+    })
+    res.status(200).json(orders)
+  } catch (err) {
+    next(err)
+  }
 })
 
 //PATCH /api/orders/:orderId - Edits an order from the database by order id
-router.patch('/:orderId', (req, res, next) => {
+router.patch('/:orderId', async (req, res, next) => {
   const { orderId } = req.params
-  const { customer, status, orderLines, totalOrder, totalTaxes, shippingFees } = req.body
-
-  Order.findByIdAndUpdate(
-    orderId,
-    { customer, status, orderLines, totalOrder, totalTaxes, shippingFees },
-    { new: true },
-  )
-    .populate('customer')
-    .then((order) => res.status(200).json(order))
-    .catch((err) => next(err))
+  const {
+    customer,
+    status,
+    orderLines,
+    totalOrder,
+    totalTaxes,
+    shippingFees,
+  } = req.body
+  try {
+    const order = await Order.findByIdAndUpdate(
+      orderId,
+      { customer, status, orderLines, totalOrder, totalTaxes, shippingFees },
+      { new: true },
+    ).populate('customer')
+    res.status(200).json(order)
+  } catch (err) {
+    next(err)
+  }
 })
 
 //DELETE /api/orders/:orderId - Deletes an order from the database by order id
-router.delete('/:orderId', (req, res, next) => {
+router.delete('/:orderId', async (req, res, next) => {
   const { orderId } = req.params
-
-  Order.findByIdAndDelete(orderId)
-    .populate('customer')
-    .then((order) => res.status(200).json(order))
-    .catch((err) => next(err))
+  try {
+    const order = await Order.findByIdAndDelete(orderId).populate('customer')
+    res.status(200).json(order)
+  } catch (err) {
+    next(err)
+  }
 })
 
 module.exports = router
